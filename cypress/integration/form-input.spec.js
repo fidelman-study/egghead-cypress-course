a1437ffb4ede81264949f45b4396aadf6a02fba5
Andrei Fidelman <fidelmanlife@gmail.com>
2019-03-02 15:43:34 +0100

    Test React’s Controlled Input with Cypress Selector Playground

--- cypress/integration/form-input.spec.js
+++ cypress/integration/form-input.spec.js
@@ -1,5 +1,14 @@
 describe('Form Input', () => {
   it('Visit the app', () => {
     cy.visit('/')
-  }) 
+    cy.focused().should('have.class', 'new-todo')
+  })
+
+  it.only('Accepts input', () => {
+    cy.visit('/')
+    cy
+      .get('.new-todo')
+      .type('New Todo')
+      .should('have.value', 'New Todo')
+  })
 })
